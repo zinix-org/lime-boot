87937e6f130c422e4014a98e6008adc3ea74cb27
Viktor Popp <viktorpopp.dev@gmail.com>
2026-04-14 21:49:31 +0200

    add bios parameter block

--- src/stage1/hdd/boot.asm
+++ src/stage1/hdd/boot.asm
@@ -20,6 +20,34 @@ bits 16         ; the assembler should emit 16-bit code
 jmp short _start
 nop
 
+;
+; IBM's El Torito Emulation tries to fix up the BPB values if they don't look
+; like regular values for a disk.
+;
+; Source: https://github.com/freebsd/freebsd-src/blob/master/stand/i386/boot2/boot1.S
+;
+bdb:
+.oem_id:                db "LIMEBOOT"
+.sector_size:           dw 512
+.sectors_per_cluster:   db 0
+.reserved_sectors:      dw 0
+.fat_count:             db 0
+.root_entries:          dw 0
+.sector_count:          dw 0
+.media_type:            db 0
+.sectors_per_fat:       dw 0
+.sectors_per_track:     dw 18   ; requied for thinkpads
+.head_count:            dw 2    ; requied for thinkpads
+.hidden_sector_count:   dd 0
+.large_sector_count:    dd 0
+ebr:
+.drive_number:          db 0
+                        db 0    ; reserved
+.signature:             db 0
+.volume_id:             db 0x12, 0x34, 0x56, 0x78
+.volume_label:          db "LIMEBOOT   "
+.filesystem_id:         db "        "   ; invalid FAT partition
+
 _start:
     cli
     cld
