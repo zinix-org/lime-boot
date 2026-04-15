aea79182261aff40343de40cb7b2c922d1995df6
Viktor Popp <viktorpopp.dev@gmail.com>
2026-04-15 16:08:15 +0200

    new build system

---
+++ docs/INSTALL.md
@@ -0,0 +1,15 @@
+# Build and Installation Instructions
+
+> [!CAUTION]
+> The following documentation is not fully implemented.
+
+This document describes how to build and install LimeBoot.
+
+## Floppy Disks
+
+To build LimeBoot stage 1 (the boot sector) for floppy disks run the following
+command:
+
+```sh
+make floppy
+```
--- src/stage1/floppy/bootsect.asm
+++ src/stage1/floppy/bootsect.asm
@@ -20,33 +20,27 @@ bits 16         ; the assembler should emit 16-bit code
 jmp short _start
 nop
 
-;
-; IBM's El Torito Emulation tries to fix up the BPB values if they don't look
-; like regular values for a disk.
-;
-; Source: https://github.com/freebsd/freebsd-src/blob/master/stand/i386/boot2/boot1.S
-;
 bdb:
-.oem_id:                db "LIMEBOOT"
+.oem_id:                db "LIMEBOOT"               ; 8 bytes        
 .sector_size:           dw 512
-.sectors_per_cluster:   db 0
-.reserved_sectors:      dw 0
-.fat_count:             db 0
-.root_entries:          dw 0
-.sector_count:          dw 0
-.media_type:            db 0
-.sectors_per_fat:       dw 0
-.sectors_per_track:     dw 18   ; requied for thinkpads
-.head_count:            dw 2    ; requied for thinkpads
+.sectors_per_cluster:   db 1
+.reserved_sectors:      dw 1
+.fat_count:             db 2
+.root_entries:          dw 0xE0
+.sector_count:          dw 2880                     ; 2880 * 512 = 1.44 MB
+.media_type:            db 0xF0                     ; 0xF0 = 3.5" floppy disk
+.sectors_per_fat:       dw 9
+.sectors_per_track:     dw 18
+.head_count:            dw 2
 .hidden_sector_count:   dd 0
 .large_sector_count:    dd 0
 ebr:
-.drive_number:          db 0
-                        db 0    ; reserved
-.signature:             db 0
-.volume_id:             db 0x12, 0x34, 0x56, 0x78
-.volume_label:          db "LIMEBOOT   "
-.filesystem_id:         db "        "   ; invalid FAT partition
+.drive_number:          db 0x00                     ; 0x00 = floppy disk
+                        db 0                        ; reserved
+.signature:             db 0x29
+.volume_id:             db 0x12, 0x34, 0x56, 0x78   ; serial number
+.volume_label:          db "LIMEBOOT   "            ; 11 bytes
+.filesystem_id:         db "FAT12   "               ; 8 bytes
 
 _start:
     cli
@@ -61,6 +55,10 @@ _start:
     mov sp, 0x7C00      ; the stack grows downwards from where we are loaded
 
     sti
+
+    ;
+    ; TODO: implement :3
+    ;
 
 halt:
     hlt
